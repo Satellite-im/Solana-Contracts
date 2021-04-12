0b90bb27bcfafc9f621ff8e0574a4f5e43934519
dzmitry.lahoda <dzmitry@lahoda.pro>
2021-04-12 20:20:20 +0300

    git push

--- servers/program/src/state.rs
+++ servers/program/src/state.rs
@@ -1,6 +1,4 @@
-///! Registry types. Registry it self is off chain API.
-/// So there should be Registry authorities which can do some operation on Dwellers stuff
-
+///! Registry types. 
 use borsh::{BorshDeserialize, BorshSchema, BorshSerialize};
 use num_derive::{FromPrimitive, ToPrimitive};
 use solana_program::pubkey::Pubkey;
@@ -29,7 +27,7 @@ pub struct Dweller {
     pub version: StateVersion,
     
     /// used to derive DwellerServer
-    pub server_index_seed: u8,
+    pub servers: u8,
 
     /// This is the display name of a dweller
     pub name: [u8;32],
@@ -37,6 +35,9 @@ pub struct Dweller {
     /// Photo identification of the dweller
     /// Multihash referencing IPFS hash of dwellers photo
     pub photo_hash: Option<[u8;64]>,    
+
+    // here could be some u8 for status before String , or brought to other account
+    pub status : String,
 }
 
 
@@ -53,19 +54,49 @@ pub struct DwellerServer {
 }
 
 
+/// Has program derived address from Server
 #[repr(C)]
 #[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone, BorshSchema)]
-pub struct ServerMemberStatus {
+pub struct ServerMember {
     pub version: StateVersion,
+    pub server:Pubkey,
     pub dweller: Pubkey,
-    // here could be some u8 for status before String 
-    pub status : String,
 }
+
+#[repr(C)]
+#[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone, BorshSchema)]
+pub struct ServerAdministrator {
+    pub version: StateVersion,
+    pub server:Pubkey,
+    pub dweller: Pubkey,
+}
+
 
 #[repr(C)]
 #[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone, BorshSchema)]
 pub struct Server {
     pub version: StateVersion,
+    /// must be dweller
+    pub owner: Pubkey,
+
+    // can join and leave Dwellers into server
+    pub authority: Pubkey,
+
+    pub name: [u8;32],
+    
+    /// Photo identification of the dweller
+    /// Multihash referencing IPFS hash of dwellers photo
+    pub photo_hash: Option<[u8;64]>,    
+
+    /// additional access hash
+    pub db_hash: Option<[u8;64]>,
+
+    // we store roles as separate account types, not as enum flag in member state as in original Solidity contract
+
+    /// Server members whom have joined, index used to derive addresses
+    pub members: u16,
+
+    pub administrators:u8,
 }
 
 
@@ -88,16 +119,10 @@ pub struct ServerGroupChannel {
     pub version: StateVersion,
 }
 
+/// Server members whom have joined
 /// many to many map of `DwellerID` to `Server` (inverse of `DwellerServer`)
 #[repr(C)]
 #[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone, BorshSchema)]
 pub struct ServerMember {
-    pub version: StateVersion,
-}
-
-
-#[repr(C)]
-#[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone, BorshSchema)]
-pub struct ServerAdministrator {
     pub version: StateVersion,
 }
