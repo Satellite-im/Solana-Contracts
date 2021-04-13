c6e187528bf13c752a18db55fe06b7c90ca3c8da
Vadim <grozinok@gmail.com>
2021-04-13 13:11:56 +0300

    Add Instruction enum and data structs to state.rs

--- friends/program/src/instruction.rs
+++ friends/program/src/instruction.rs
@@ -5,27 +5,241 @@ use solana_program::{
     instruction::{AccountMeta, Instruction},
     program_error::ProgramError,
     pubkey::Pubkey,
+    sysvar,
 };
 
 /// Instruction definition
 #[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone)]
 pub enum FriendsInstruction {
-    /// Example.
+    /// InitFriendInfo
     ///
-    ///   0. `[w]` Example account.
-    ExampleInstruction,
+    ///   0. `[w]` Uninitialized FriendInfo account
+    ///   1. `[rs]` User's account
+    ///   2. `[r]` Rent sysvar
+    InitFriendInfo,
+
+    /// MakeRequest
+    ///
+    ///   0. `[w]` Friendship request for "from" account
+    ///   1. `[w]` Friendship request for "to" account
+    ///   2. `[w]` Friend info of account which request friendship
+    ///   3. `[w]` Friend info of account with which friendship requested
+    ///   4. `[rs]` friend_info_from's "user" key. To verify friendship request
+    MakeRequest,
+
+    /// AcceptRequest
+    ///
+    ///   0. `[w]` Friendship request for "from" account
+    ///   1. `[w]` Friendship request for "to" account
+    ///   2. `[w]` Friend info of account which request friendship
+    ///   3. `[w]` Friend info of account with which friendship requested
+    ///   4. `[w]` Uninitialized Friend account for "to" account
+    ///   5. `[w]` Uninitialized Friend account for "from" account
+    ///   6. `[rs]` friend_info_to's "user" key. To verify acception side
+    AcceptRequest([u8; 32], [u8; 32]),
+
+    /// DenyRequest
+    ///
+    ///   0. `[w]` Friendship request for "from" account
+    ///   1. `[w]` Friendship request for "to" account
+    ///   2. `[w]` Last friendship request for "from" account
+    ///   3. `[w]` Last friendship request for "to" account
+    ///   4. `[w]` Friend info of account which request friendship
+    ///   5. `[w]` Friend info of account with which friendship requested
+    ///   6. `[rs]` friend_info_to's "user" key. To verify acception side
+    DenyRequest,
+
+    /// RemoveRequest
+    ///
+    ///   0. `[w]` Friendship request for "from" account
+    ///   1. `[w]` Friendship request for "to" account
+    ///   2. `[w]` Last friendship request for "from" account
+    ///   3. `[w]` Last friendship request for "to" account
+    ///   4. `[w]` Friend info of account which request friendship
+    ///   5. `[w]` Friend info of account with which friendship requested
+    ///   6. `[rs]` friend_info_from's "user" key. To verify requesting side
+    RemoveRequest,
+
+    /// RemoveFriend
+    ///
+    ///   0. `[w]` Friend info of account which wants to break friendship
+    ///   1. `[w]` Friend info of account with which wants to break friendship
+    ///   2. `[w]` Friend account which wants to break friendship
+    ///   3. `[w]` Friend account with which wants to break friendship
+    ///   4. `[w]` Last friend account of account which initiate friendship break
+    ///   5. `[w]` Last friend account of account with which wants break friendship
+    ///   6. `[rs]` User account which initiate break friendship
+    RemoveFriend,
 }
 
-/// Create `Example` instruction
+/// Create `InitFriendInfo` instruction
 pub fn init(
     program_id: &Pubkey,
-    example_account: &Pubkey,
+    friend_info: &Pubkey,
+    user: &Pubkey,
 ) -> Result<Instruction, ProgramError> {
-    let init_data = FriendsInstruction::ExampleInstruction;
+    let init_data = FriendsInstruction::InitFriendInfo;
     let data = init_data
         .try_to_vec()
         .or(Err(ProgramError::InvalidArgument))?;
-    let accounts = vec![AccountMeta::new(*example_account, false)];
+    let accounts = vec![
+        AccountMeta::new(*friend_info, false),
+        AccountMeta::new_readonly(*user, true),
+        AccountMeta::new_readonly(sysvar::rent::id(), false),
+    ];
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+}
+
+/// Create `MakeRequest` instruction
+pub fn make_request(
+    program_id: &Pubkey,
+    request_from_to: &Pubkey,
+    request_to_from: &Pubkey,
+    friend_info_from: &Pubkey,
+    friend_info_to: &Pubkey,
+    user_from: &Pubkey,
+) -> Result<Instruction, ProgramError> {
+    let init_data = FriendsInstruction::MakeRequest;
+    let data = init_data
+        .try_to_vec()
+        .or(Err(ProgramError::InvalidArgument))?;
+    let accounts = vec![
+        AccountMeta::new(*request_from_to, false),
+        AccountMeta::new(*request_to_from, false),
+        AccountMeta::new(*friend_info_from, false),
+        AccountMeta::new(*friend_info_to, false),
+        AccountMeta::new_readonly(*user_from, true),
+    ];
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+}
+
+/// Create `AcceptRequest` instruction
+pub fn accept_request(
+    program_id: &Pubkey,
+    request_from_to: &Pubkey,
+    request_to_from: &Pubkey,
+    friend_info_from: &Pubkey,
+    friend_info_to: &Pubkey,
+    friend_to: &Pubkey,
+    friend_from: &Pubkey,
+    user_to: &Pubkey,
+    thread_id1: [u8; 32],
+    thread_id2: [u8; 32],
+) -> Result<Instruction, ProgramError> {
+    let init_data = FriendsInstruction::AcceptRequest(thread_id1, thread_id2);
+    let data = init_data
+        .try_to_vec()
+        .or(Err(ProgramError::InvalidArgument))?;
+    let accounts = vec![
+        AccountMeta::new(*request_from_to, false),
+        AccountMeta::new(*request_to_from, false),
+        AccountMeta::new(*friend_info_from, false),
+        AccountMeta::new(*friend_info_to, false),
+        AccountMeta::new(*friend_to, false),
+        AccountMeta::new(*friend_from, false),
+        AccountMeta::new_readonly(*user_to, true),
+    ];
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+}
+
+/// Create `DenyRequest` instruction
+pub fn deny_request(
+    program_id: &Pubkey,
+    request_from_to: &Pubkey,
+    request_to_from: &Pubkey,
+    last_request_from: &Pubkey,  // last outgoing request for requested account
+    last_request_to: &Pubkey,  // last incoming request for denied account
+    friend_info_from: &Pubkey,
+    friend_info_to: &Pubkey,
+    user_to: &Pubkey,
+) -> Result<Instruction, ProgramError> {
+    let init_data = FriendsInstruction::DenyRequest;
+    let data = init_data
+        .try_to_vec()
+        .or(Err(ProgramError::InvalidArgument))?;
+    let accounts = vec![
+        AccountMeta::new(*request_from_to, false),
+        AccountMeta::new(*request_to_from, false),
+        AccountMeta::new(*last_request_from, false),
+        AccountMeta::new(*last_request_to, false),
+        AccountMeta::new(*friend_info_from, false),
+        AccountMeta::new(*friend_info_to, false),
+        AccountMeta::new_readonly(*user_to, true),
+    ];
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+}
+
+/// Create `RemoveRequest` instruction
+pub fn remove_request(
+    program_id: &Pubkey,
+    request_from_to: &Pubkey,
+    request_to_from: &Pubkey,
+    last_request_from: &Pubkey,  // last outgoing request for requested account
+    last_request_to: &Pubkey,  // last incoming request for denied account
+    friend_info_from: &Pubkey,
+    friend_info_to: &Pubkey,
+    user_from: &Pubkey,
+) -> Result<Instruction, ProgramError> {
+    let init_data = FriendsInstruction::RemoveRequest;
+    let data = init_data
+        .try_to_vec()
+        .or(Err(ProgramError::InvalidArgument))?;
+    let accounts = vec![
+        AccountMeta::new(*request_from_to, false),
+        AccountMeta::new(*request_to_from, false),
+        AccountMeta::new(*last_request_from, false),
+        AccountMeta::new(*last_request_to, false),
+        AccountMeta::new(*friend_info_from, false),
+        AccountMeta::new(*friend_info_to, false),
+        AccountMeta::new_readonly(*user_from, true),
+    ];
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+}
+
+/// Create `RemoveFriend` instruction
+pub fn remove_friend(
+    program_id: &Pubkey,
+    friend_info_first: &Pubkey,
+    friend_info_second: &Pubkey,
+    friend_first: &Pubkey,
+    friend_second: &Pubkey,
+    last_friend_of_first: &Pubkey,
+    last_friend_of_second: &Pubkey,
+    user: &Pubkey,
+) -> Result<Instruction, ProgramError> {
+    let init_data = FriendsInstruction::RemoveFriend;
+    let data = init_data
+        .try_to_vec()
+        .or(Err(ProgramError::InvalidArgument))?;
+    let accounts = vec![
+        AccountMeta::new(*friend_info_first, false),
+        AccountMeta::new(*friend_info_second, false),
+        AccountMeta::new(*friend_first, false),
+        AccountMeta::new(*friend_second, false),
+        AccountMeta::new(*last_friend_of_first, false),
+        AccountMeta::new(*last_friend_of_second, false),
+        AccountMeta::new_readonly(*user, true),
+    ];
     Ok(Instruction {
         program_id: *program_id,
         accounts,
--- friends/program/src/state.rs
+++ friends/program/src/state.rs
@@ -1,1 +1,39 @@
 //! State transition types
+
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::pubkey::Pubkey;
+
+/// Friend info
+#[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone)]
+pub struct FriendInfo {
+    /// User key
+    pub user: Pubkey,
+    /// Count of incoming friend requests
+    pub requests_incoming: u64,
+    /// Count of outgoing friend requests
+    pub requests_outgoing: u64,
+    /// Count of friends
+    pub friends: u64,
+}
+
+/// Friend request
+#[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone)]
+pub struct Request {
+    /// From key
+    pub from: Pubkey,
+    /// To key
+    pub to: Pubkey,
+}
+
+/// Friend
+#[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone)]
+pub struct Friend {
+    /// User key
+    pub user: Pubkey,
+    /// Friend key
+    pub friend: Pubkey,
+    /// Conversation thread hash 1
+    pub thread_id1: [u8; 32],
+    /// Conversation thread hash 2
+    pub thread_id2: [u8; 32],
+}
