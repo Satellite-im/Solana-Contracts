916e19f2f16661af8c62e62b6d14ce97d85b8ef9
Vadim <grozinok@gmail.com>
2021-04-13 11:27:36 +0300

    Init Friends program

---
+++ friends/program/src/instruction.rs
@@ -0,0 +1,34 @@
+//! Instruction types
+
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    instruction::{AccountMeta, Instruction},
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+/// Instruction definition
+#[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone)]
+pub enum FriendsInstruction {
+    /// Example.
+    ///
+    ///   0. `[w]` Example account.
+    ExampleInstruction,
+}
+
+/// Create `Example` instruction
+pub fn init(
+    program_id: &Pubkey,
+    example_account: &Pubkey,
+) -> Result<Instruction, ProgramError> {
+    let init_data = FriendsInstruction::ExampleInstruction;
+    let data = init_data
+        .try_to_vec()
+        .or(Err(ProgramError::InvalidArgument))?;
+    let accounts = vec![AccountMeta::new(*example_account, false)];
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+}
